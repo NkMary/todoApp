4abab59b8fd1e3aaed38bbfe6085319351ce0a35
Nkechinyere Urum-Eke <nk@nkechinyeresmbp.myfiosgateway.com>
2023-07-20 18:46:17 -0400

    created an array of todos and completed tasks

---
+++ models/data.js
@@ -0,0 +1,6 @@
+require('dotenv').config()
+
+const tasks = ["practice violin", "study", "schedule Mia's appointment", "call dad", "go to the gym", "work on resume"]:
+const complete = ["finish cooking", "finish cleaning"];
+
+module.exports = {tasks, complete}
